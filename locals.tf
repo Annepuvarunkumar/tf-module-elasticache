66b8d9976b8d1c31fa6e081b3f2154a874aa60b1
varun <varunannepu123@gmail.com>
2023-12-29 21:31:45 +0530

    rds cluster

---
+++ locals.tf
@@ -0,0 +1,7 @@
+locals {
+  name_prefix   = "${var.env}-${var.elasticache_type}-rds"
+  tags          = merge(var.tags, { tf-module-name = "rds" }, { env = var.env })
+
+}
+
+
---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "env" {}
+variable "tags" {}
+variable "elasticache_type" {}
+variable "subnet_ids" {}
+variable "family" {}
+variable "vpc_id" {}
+variable "port" {}
+variable "sg_ingress_cidr" {}
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
